688f492c93774226b64796842b15ca74b30f39f3
Vincius Rodrigues <viniro3201@gmail.com>
2019-02-18 11:04:48 -0300

    MongoDb PORT change

--- back-end/mean-example/config/default.ts
+++ back-end/mean-example/config/default.ts
@@ -1,6 +1,6 @@
 export default {
   HOST: 'http://localhost',
   PORT: 3000,
-  MONGO_URI: 'mongodb://mean-example:password123@mongodb:27017/mean-example',
+  MONGO_URI: 'mongodb://mean-example:password123@mongodb:27018/mean-example',
   JWT_KEY: 'MYSECRET',
 };
